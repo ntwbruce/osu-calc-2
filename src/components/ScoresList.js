a30f5008cf58f06d1f00c729acc514b828ef785b
TechnoTew <jingan0904@gmail.com>
2023-06-13 21:21:03 +0800

    Update ScoresList.js
    - show "no scores found" when scorelist data is an empty array

--- src/components/ScoresList.js
+++ src/components/ScoresList.js
@@ -2,6 +2,11 @@ import Score from "./Score";
 import styles from './ScoresList.module.css';
 
 function ScoresList({data}) {
+  if (data.length === 0) {
+    return (
+      <h3>No scores found.</h3>
+    )
+  }
   return data.map((scoreData, idx) => <Score key={idx} idx={idx} scoreData={scoreData} />);
 }
 
