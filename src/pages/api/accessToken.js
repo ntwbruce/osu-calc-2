4f1eb4a18d2c1aad8ed6fd5cdcfee619acc24ea0
TechnoTew <jingan0904@gmail.com>
2023-06-13 21:22:44 +0800

    Move API call to query access token into NextJS API route

---
+++ src/pages/api/accessToken.js
@@ -0,0 +1,38 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { sessionOptions } from "@/lib/session";
+import axios from "axios";
+
+export default withIronSessionApiRoute(
+    async function getAccessToken(req, res) {
+        try {
+            // restrict requests to be only POST
+            if (req.method !== "POST") {
+                res.status(405).send({ message: "Only POST requests allowed" })
+                return;
+            }
+
+            // check if accessToken is present and not expired, skip requesting for a new one
+            if (req.session.accessToken && req.session.accessTokenExpiry > (new Date()).getTime()) {
+                return res.status(200).send({ message: "Access token already present. Skipping request." })
+            }
+
+            // obtain authToken from client_id and client_secret
+            const auth_data = (await axios.post("https://osu.ppy.sh/oauth/token", {
+                client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+                client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+                grant_type: "client_credentials",
+                scope: "public"
+            })).data;
+
+            req.session.accessToken = auth_data.access_token;
+            req.session.accessTokenExpiry = (new Date()).getTime() + 1000 * auth_data.expires_in;
+
+            await req.session.save();
+            res.status(200).send({ message: "Access token retrieved successfully" });
+        } catch (error) {
+            console.log("Error fetching access token: " + error);
+            res.status(error.response.status).send("Error fetching access token: " + error);
+        }
+    },
+    sessionOptions
+);
