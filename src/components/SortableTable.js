4c75492cd3ca07428fbcb8fe3bef23505cababad
ntwbruce <e0544240@u.nus.edu>
2023-07-22 16:38:42 +0800

    Remove unused imports

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,10 +1,6 @@
 import { useEffect, useState } from "react";
 import {
   createStyles,
-  UnstyledButton,
-  Group,
-  Text,
-  Center,
   TextInput,
   rem,
   Flex,
@@ -17,9 +13,6 @@ import {
   Loader,
 } from "@mantine/core";
 import {
-  IconSelector,
-  IconChevronDown,
-  IconChevronUp,
   IconSearch,
   IconFilter,
   IconSortAscending,
@@ -28,30 +21,6 @@ import {
 } from "@tabler/icons-react";
 import { useDisclosure } from "@mantine/hooks";
 import Score from "./Score";
-
-const useStyles = createStyles((theme) => ({
-  th: {
-    padding: "0 !important",
-  },
-
-  control: {
-    width: "100%",
-    padding: `${theme.spacing.xs} ${theme.spacing.md}`,
-
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-    },
-  },
-
-  icon: {
-    width: rem(21),
-    height: rem(21),
-    borderRadius: rem(21),
-  },
-}));
 
 /**
  * Filters data by given search parameters object.
