b510fcbf18d9c0565d7e1e0b989c267815c85632
ntwbruce <e0544240@u.nus.edu>
2023-08-08 04:54:02 +0800

    Add recharts and rank graph

--- src/lib/calculators/DateCalculator.js
+++ src/lib/calculators/DateCalculator.js
@@ -1,6 +1,6 @@
 // Calculate end date based on a start date and a duration.
 export const calculateDate = (startDate, days) => {
     let endDate = new Date(startDate);
-    endDate.setDate(startDate.getDate() + 1);
+    endDate.setDate(startDate.getDate() + days);
     return endDate;
 };
--- src/pages/[username]/index.js
+++ src/pages/[username]/index.js
@@ -15,7 +15,15 @@ import {
 import Head from "next/head";
 import { IconHammer, IconZoomQuestion } from "@tabler/icons-react";
 import { HeaderBar } from "@/components/HeaderBar";
-import { Cell, Pie, PieChart } from "recharts";
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { calculateDate } from "@/lib/calculators/DateCalculator";
 
 export default function UserProfilePage() {
   const router = useRouter();
@@ -48,6 +56,9 @@ export default function UserProfilePage() {
   const [hitData, setHitData] = useState({});
   const [isHitDataSet, setIsHitDataSet] = useState(false);
 
+  const [rankHistoryData, setRankHistoryData] = useState({});
+  const [isRankHistoryDataSet, setIsRankHistoryDataSet] = useState(false);
+
   useEffect(() => {
     if (Object.keys(userData).length !== 0) {
       setHitData({
@@ -58,6 +69,31 @@ export default function UserProfilePage() {
         total: userData.statistics.total_hits + userData.statistics.count_miss,
       });
       setIsHitDataSet(true);
+
+      const today = new Date();
+      let minRank = -1;
+      let minRankIdx = -1;
+      let maxRank = -1;
+      let maxRankIdx = -1;
+
+      const history = userData.rank_history.data.map((rank, index) => {
+        if (rank > minRank || minRank === -1) {
+          minRank = rank;
+          minRankIdx = index;
+        }
+        if (rank < maxRank || maxRank === -1) {
+          maxRank = rank;
+          maxRankIdx = index;
+        }
+        return {
+          rank,
+          index,
+          date: calculateDate(today, index - 89).toLocaleDateString("en-SG"),
+        };
+      });
+
+      setRankHistoryData({ history, minRank, minRankIdx, maxRank, maxRankIdx });
+      setIsRankHistoryDataSet(true);
     }
   }, [userData]);
 
@@ -93,9 +129,94 @@ export default function UserProfilePage() {
     }
   }, [router.isReady, authTokenPresent]);
 
+  // ============================================= BEST SCORES DATA FETCHING =============================================
+
+  const [bestScoresData, setBestScoresData] = useState({});
+  const [isBestScoresDataSet, setIsBestScoresDataSet] = useState(false);
+
+  async function fetchBestScoresDataHandler() {
+    try {
+      const response = (
+        await axios.get(
+          `/api/users/${userData.id}/scores/best?limit=${userData.scores_best_count}`
+        )
+      ).data;
+      setBestScoresData(response.data);
+      setIsBestScoresDataSet(true);
+    } catch (error) {
+      console.log("error fetching best scores: " + error.response.data);
+      setBestScoresData([]);
+      setIsBestScoresDataSet(false);
+    }
+  }
+
+  // * Fetch best score data
+  useEffect(() => {
+    if (isUserDataSet) {
+      fetchBestScoresDataHandler();
+    }
+  }, [isUserDataSet]);
+
   // ============================================= OUTPUT =============================================
 
-  const COLORS = ["#4287f5", "#42f560", "#cef542", "#c73c3c"];
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <Paper
+          shadow="sm"
+          p="sm"
+          sx={{ outline: "solid", borderRadius: "10px", color: "white" }}
+          bg="rgba(50, 50, 50, .6)"
+        >
+          <Title order={6}>{`Global Rank #${payload[0].value}`}</Title>
+          <Title order={6}>{`${label} - ${
+            payload[0].payload.index === 89
+              ? "today"
+              : `${89 - payload[0].payload.index} days ago`
+          }`}</Title>
+        </Paper>
+      );
+    }
+
+    return null;
+  };
+
+  const CustomizedDot = ({ cx, cy, payload, value }) => {
+    if (
+      value === rankHistoryData.maxRank &&
+      payload.index === rankHistoryData.maxRankIdx
+    ) {
+      return (
+        <circle
+          cx={cx}
+          cy={cy}
+          r={5}
+          stroke="black"
+          strokeWidth={2}
+          fill="lime"
+        />
+      );
+    }
+    if (
+      (rankHistoryData.minRank !== rankHistoryData.maxRank ||
+        rankHistoryData.minRankIdx !== rankHistoryData.maxRankIdx) &&
+      value === rankHistoryData.minRank &&
+      payload.index === rankHistoryData.minRankIdx
+    ) {
+      {
+        return (
+          <circle
+            cx={cx}
+            cy={cy}
+            r={5}
+            stroke="black"
+            strokeWidth={2}
+            fill="red"
+          />
+        );
+      }
+    }
+  };
 
   return (
     <>
@@ -126,62 +247,95 @@ export default function UserProfilePage() {
           />
         )}
 
-        {isUserDataSet && isHitDataSet && (
+        {isUserDataSet && (
           <>
             <Flex direction="column" align="center">
               <Flex direction="row">
-                <RingProgress
-                  size={260}
-                  thickness={30}
-                  label={
-                    <>
-                      <Title order={3} align="center">
-                        Total Hits and Misses
-                      </Title>
-                      <Text align="center">
-                        {hitData.total.toLocaleString()}
-                      </Text>
-                    </>
-                  }
-                  sections={[
-                    {
-                      value: (hitData.count_300 / hitData.total) * 96,
-                      color: COLORS[0],
-                      tooltip: `300s: ${hitData.count_300.toLocaleString()} - ${(
-                        (hitData.count_300 / hitData.total) *
-                        100
-                      ).toFixed(2)}%`,
-                    },
-                    { value: 1, color: "black" },
-                    {
-                      value: (hitData.count_100 / hitData.total) * 96,
-                      color: COLORS[1],
-                      tooltip: `100s: ${hitData.count_100.toLocaleString()} - ${(
-                        (hitData.count_100 / hitData.total) *
-                        100
-                      ).toFixed(2)}%`,
-                    },
-                    { value: 1, color: "black" },
-                    {
-                      value: (hitData.count_50 / hitData.total) * 96,
-                      color: COLORS[2],
-                      tooltip: `50s: ${hitData.count_50.toLocaleString()} - ${(
-                        (hitData.count_50 / hitData.total) *
-                        100
-                      ).toFixed(2)}%`,
-                    },
-                    { value: 1, color: "black" },
-                    {
-                      value: (hitData.count_miss / hitData.total) * 96,
-                      color: COLORS[3],
-                      tooltip: `Misses: ${hitData.count_miss.toLocaleString()} - ${(
-                        (hitData.count_miss / hitData.total) *
-                        100
-                      ).toFixed(2)}%`,
-                    },
-                    { value: 1, color: "black" },
-                  ]}
-                />
+                {isHitDataSet && (
+                  <RingProgress
+                    size={260}
+                    thickness={30}
+                    label={
+                      <>
+                        <Title order={3} align="center">
+                          Total Hits and Misses
+                        </Title>
+                        <Text align="center">
+                          {hitData.total.toLocaleString()}
+                        </Text>
+                      </>
+                    }
+                    sections={[
+                      {
+                        value: (hitData.count_300 / hitData.total) * 96,
+                        color: "#4287f5",
+                        tooltip: `300s: ${hitData.count_300.toLocaleString()} - ${(
+                          (hitData.count_300 / hitData.total) *
+                          100
+                        ).toFixed(2)}%`,
+                      },
+                      { value: 1, color: "black" },
+                      {
+                        value: (hitData.count_100 / hitData.total) * 96,
+                        color: "#42f560",
+                        tooltip: `100s: ${hitData.count_100.toLocaleString()} - ${(
+                          (hitData.count_100 / hitData.total) *
+                          100
+                        ).toFixed(2)}%`,
+                      },
+                      { value: 1, color: "black" },
+                      {
+                        value: (hitData.count_50 / hitData.total) * 96,
+                        color: "#cef542",
+                        tooltip: `50s: ${hitData.count_50.toLocaleString()} - ${(
+                          (hitData.count_50 / hitData.total) *
+                          100
+                        ).toFixed(2)}%`,
+                      },
+                      { value: 1, color: "black" },
+                      {
+                        value: (hitData.count_miss / hitData.total) * 96,
+                        color: "#c73c3c",
+                        tooltip: `Misses: ${hitData.count_miss.toLocaleString()} - ${(
+                          (hitData.count_miss / hitData.total) *
+                          100
+                        ).toFixed(2)}%`,
+                      },
+                      { value: 1, color: "black" },
+                    ]}
+                  />
+                )}
+                {isRankHistoryDataSet && (
+                  <LineChart
+                    width={730}
+                    height={250}
+                    data={rankHistoryData.history}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" vertical={false} />
+                    <XAxis
+                      dataKey="date"
+                      stroke="#ffffff"
+                      padding={{ left: 10, right: 10 }}
+                      hide
+                    />
+                    <YAxis
+                      stroke="#ffffff"
+                      domain={["auto", "auto"]}
+                      reversed
+                      interval="preserveStartEnd"
+                    />
+                    <Tooltip content={<CustomTooltip />} />
+                    <Line
+                      type="monotone"
+                      dataKey="rank"
+                      stroke="#daa520"
+                      strokeWidth={3}
+                      dot={<CustomizedDot />}
+                    />
+                  </LineChart>
+                )}
+
                 <ScrollArea h="25vh">
                   <Title order={4}>WIP things to add</Title>
                   <Title order={4}>
@@ -206,19 +360,24 @@ export default function UserProfilePage() {
                     {userData.rank_highest.updated_at}
                   </Title>
                   <Title order={4}>
-                    level {userData.statistics.level.current} + {userData.statistics.level.progress}%
+                    level {userData.statistics.level.current} +{" "}
+                    {userData.statistics.level.progress}%
                   </Title>
                   <Title order={4}>
-                    max combo {userData.statistics.maximum_combo.toLocaleString()}
+                    max combo{" "}
+                    {userData.statistics.maximum_combo.toLocaleString()}
                   </Title>
                   <Title order={4}>
-                    playtime {userData.statistics.play_time.toLocaleString()} seconds
+                    playtime {userData.statistics.play_time.toLocaleString()}{" "}
+                    seconds
                   </Title>
                   <Title order={4}>
-                    ranked score {userData.statistics.ranked_score.toLocaleString()}
+                    ranked score{" "}
+                    {userData.statistics.ranked_score.toLocaleString()}
                   </Title>
                   <Title order={4}>
-                    total score {userData.statistics.total_score.toLocaleString()}
+                    total score{" "}
+                    {userData.statistics.total_score.toLocaleString()}
                   </Title>
                 </ScrollArea>
               </Flex>
